08ea0d5cfae3f638f12f282633a27ee97eddd9f4
tuetranduy <tue1996@gmail.com>
2018-02-25 11:33:55 +0700

    update minor

---
+++ ecosystem.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  apps: [{
+    name: 'pbs',
+    script: './index.js'
+  }],
+  deploy: {
+    production: {
+      user: 'bitnami',
+      host: '52.23.184.144',
+      key: 'C:/Users/tue19/.ssh/server.pem',
+      ref: 'origin/master',
+      repo: 'git@github.com:tuetranduy/pbs-nodeJs-server.git',
+      path: '~/server',
+      'post-deploy': 'npm install && sudo pm2 startOrRestart ecosystem.config.js'
+    }
+  }
+}
