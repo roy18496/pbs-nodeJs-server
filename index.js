02bbb08bb99d1b30560cdaca5576c4a3aa80ffe5
tuetranduy <tue1996@gmail.com>
2018-02-25 12:41:44 +0700

    add index.html

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-app.get('/', (req, res) => {
-    res.send('Server is up')
-})
+
+app.use(express.static("public"))
+
 app.listen(3000, () => console.log('Server running on port 3000'))
