d2ad1122d58dc08c91945dc2385776225510f751
tuetranduy <tue1996@gmail.com>
2018-02-27 23:33:33 +0700

    fix bugs

--- index.js
+++ index.js
@@ -1,17 +1,16 @@
 var express = require("express");
+var app = express();
 var server = require("http").createServer(app);
 var io = require("socket.io").listen(server);
 
-var app = express();
-
 app.use(express.static("app"));
-server.listen(process.env.PORT || 3000, () => console.log("server is up"));
+server.listen(process.env.PORT || 3000, ()=> console.log("server is up"));
 
 io.sockets.on('connection', function (socket) {
-
+	
     console.log("NOTICE: New user connected");
-    socket.on("SEND_BARCODE", function (data) {
+    socket.on("SEND_BARCODE", function(data){
         console.log(data);
     })
-
-});
+    
+  }); 
