a6964ad1dcd85d708eda97dae89e2ddbf4fe75c2
tuetranduy <tue1996@gmail.com>
2018-02-27 23:28:22 +0700

    add html

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 var express = require("express");
-var app = express();
 var server = require("http").createServer(app);
 var io = require("socket.io").listen(server);
-var fs = require("fs");
-server.listen(process.env.PORT || 3000, ()=> console.log("server is up"));
+
+var app = express();
+
+app.use(express.static("app"));
+server.listen(process.env.PORT || 3000, () => console.log("server is up"));
 
 io.sockets.on('connection', function (socket) {
-	
+
     console.log("NOTICE: New user connected");
-    socket.on("SEND_BARCODE", function(data){
+    socket.on("SEND_BARCODE", function (data) {
         console.log(data);
     })
-    
-  }); 
+
+});
