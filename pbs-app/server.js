5d9c517efeac7428ca176202e31aa7edfe377b02
tuetranduy <tue1996@gmail.com>
2018-03-11 09:46:44 +0700

    update minor

--- pbs-app/server.js
+++ pbs-app/server.js
@@ -1,16 +1,6 @@
 var express = require("express");
 var app = express();
 var server = require("http").createServer(app);
-var io = require("socket.io").listen(server);
 
 app.use(express.static("app"));
 server.listen(process.env.PORT || 3000, ()=> console.log("server is up"));
-
-io.sockets.on("connection", function (socket) {
-	
-    console.log("NOTICE: New user connected");
-    socket.on("SEND_BARCODE", function(data){
-        console.log(data);
-    })
-    
-  }); 
