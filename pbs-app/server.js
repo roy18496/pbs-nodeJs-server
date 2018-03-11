4dc58f2b29bc4273a7f3d84278a7fbfdc5c7607d
tuetranduy <tue1996@gmail.com>
2018-03-11 09:13:24 +0700

    init api

--- pbs-app/server.js
+++ pbs-app/server.js
@@ -6,7 +6,7 @@ var io = require("socket.io").listen(server);
 app.use(express.static("app"));
 server.listen(process.env.PORT || 3000, ()=> console.log("server is up"));
 
-io.sockets.on('connection', function (socket) {
+io.sockets.on("connection", function (socket) {
 	
     console.log("NOTICE: New user connected");
     socket.on("SEND_BARCODE", function(data){
