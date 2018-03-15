2e1ca439b662a628831ce9f160d223420fc863f9
tuetranduy <tue1996@gmail.com>
2018-03-15 10:04:05 +0700

    fix production

--- pbs-api/src/config/env.config.js
+++ pbs-api/src/config/env.config.js
@@ -1,9 +1,4 @@
 export default {
-	production: {
-		// host: "54.209.171.72",
-		port: 3001,
-		secretKey: "AT595r768gqHdSLqzew746hNp5VrpN7a"
-	},
 	development: {
 		host: "localhost",
 		port: 3001,
