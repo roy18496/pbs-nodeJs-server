5c842c3c89c17fc788c5773f55dad9cce6852e05
tuetranduy <tue1996@gmail.com>
2018-03-15 09:43:05 +0700

    update production config

--- pbs-api/src/config/env.config.js
+++ pbs-api/src/config/env.config.js
@@ -1,6 +1,6 @@
 export default {
 	production: {
-		host: "54.209.171.72",
+		// host: "54.209.171.72",
 		port: 3001,
 		secretKey: "AT595r768gqHdSLqzew746hNp5VrpN7a"
 	},
--- pbs-api/xclap.js
+++ pbs-api/xclap.js
@@ -6,8 +6,8 @@ const tasks = {
 		desc: "check format of code by eslint then start",
 		task: ["lint", "start"]
 	},
-	start: "node node_modules\\nodemon\\bin\\nodemon.js api.js",	
-	lint: "node node_modules\\eslint\\bin\\eslint.js src --ext js"	
+	start: "node node_modules\\nodemon\\bin\\nodemon.js api.js",
+	lint: "node node_modules\\eslint\\bin\\eslint.js src --ext js"
 };
 
 xclap.load(tasks);
